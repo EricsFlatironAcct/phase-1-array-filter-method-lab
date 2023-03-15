f6b5a1dfa57b1e27a105442ea75a5daefaec1952
Eric Krukowski <erickruk@amazon.com>
2023-03-15 01:15:27 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+const driverObjects = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+  const driverArray = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(names, reg){
+    return names.filter(fn = driver =>driver.toLowerCase()===reg.toLowerCase())
+}
+//console.log(findMatching(driverArray, "Bobby"));
+
+function fuzzyMatch(names, reg){
+    return names.filter(fn = driver =>driver.toLowerCase().slice(0,reg.length) === reg.toLowerCase())
+}
+//console.log(findMatching(driverArray, "Sa"));
+function matchName(names, reg){
+    return names.filter(fn = driver => driver.name.toLowerCase()=== reg.toLowerCase())
+}
